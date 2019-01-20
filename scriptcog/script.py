140f873d35d6f11e59fc1273b8a345b35d1bfede
brandons209 <brandons209@aol.com>
2019-01-20 01:54:06 -0500

    more bot help

--- scriptcog/script.py
+++ scriptcog/script.py
@@ -120,7 +120,7 @@ class ScriptCog:
 
     @commands.command(pass_context=True, no_pm=True)
     async def genscripthelp(self, ctx):
-        await self.bot.say("--------------------\nGenerate original TV scripts for {} using Neural Networks!\nUsage: `genscript number_of_words_to_generate word_variance starting_text`\nUse starting texts such as:\n`pinkie pie::`\n`fluttershy::`\n`twilight sparkle::`\nor other names of characters in the show. Otherwise, you can use any words said in the show.\nWord variance helps gives the generate better results. A variance of 0 will mean that with the same starting text, it will always have the same output. Variance up to 1.0 will give more variety to words, however going closer to 1 can introduce more grammar and spelling mistakes.\n-------------------".format(self.tv_show))
+        await self.bot.say("--------------------\nGenerate original TV scripts for {} using Neural Networks!\nUsage: `genscript number_of_words_to_generate word_variance starting_text`\nUse starting texts such as:\n`"pinkie pie::"`\n`fluttershy::`\n`"twilight sparkle::"`\nor other names of characters in the show. Otherwise, you can use any words said in the show.\n**If your starting text has spaces, enclose it with quotes: ""**\nWord variance helps gives the generate better results. A variance of 0 will mean that with the same starting text, it will always have the same output. Variance up to 1.0 will give more variety to words, however going closer to 1 can introduce more grammar and spelling mistakes.b\n-------------------".format(self.tv_show))
 
     @commands.command(pass_context=True, no_pm=True)
     @checks.is_owner()
